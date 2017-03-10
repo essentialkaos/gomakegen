2d1c26bf9b0251a4b12b51192c168cb45e8b205a
Anton Novojilov <andy@essentialkaos.com>
2017-03-10 16:44:03 -0500

    Improvements

--- gomakegen.go
+++ gomakegen.go
@@ -23,6 +23,7 @@ import (
 	"pkg.re/essentialkaos/ek.v7/fmtc"
 	"pkg.re/essentialkaos/ek.v7/fsutil"
 	"pkg.re/essentialkaos/ek.v7/path"
+	"pkg.re/essentialkaos/ek.v7/sliceutil"
 	"pkg.re/essentialkaos/ek.v7/usage"
 	"pkg.re/essentialkaos/ek.v7/usage/update"
 )
@@ -31,26 +32,54 @@ import (
 
 const (
 	APP  = "gomakegen"
-	VER  = "0.0.1"
+	VER  = "0.2.0"
 	DESC = "Utility for generating makefiles for golang applications"
 )
 
 const (
-	ARG_OUTPUT   = "o:output"
-	ARG_NO_COLOR = "nc:no-color"
-	ARG_HELP     = "h:help"
-	ARG_VER      = "v:version"
+	ARG_GLIDE      = "g:glade"
+	ARG_METALINTER = "m:metalinter"
+	ARG_BENCHMARK  = "b:benchmark"
+	ARG_VERB_TESTS = "V:verbose"
+	ARG_OUTPUT     = "o:output"
+	ARG_NO_COLOR   = "nc:no-color"
+	ARG_HELP       = "h:help"
+	ARG_VER        = "v:version"
 )
 
 const SEPARATOR = "########################################################################################"
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
+type Makefile struct {
+	BaseImports []string
+	TestImports []string
+	Binaries    []string
+
+	HasTests   bool
+	Benchmark  bool
+	VerbTests  bool
+	Glide      bool
+	Metalinter bool
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
 var argMap = arg.Map{
-	ARG_OUTPUT:   {Value: "Makefile"},
-	ARG_NO_COLOR: {Type: arg.BOOL},
-	ARG_HELP:     {Type: arg.BOOL, Alias: "u:usage"},
-	ARG_VER:      {Type: arg.BOOL, Alias: "ver"},
+	ARG_OUTPUT:     {Value: "Makefile"},
+	ARG_GLIDE:      {Type: arg.BOOL},
+	ARG_METALINTER: {Type: arg.BOOL},
+	ARG_BENCHMARK:  {Type: arg.BOOL},
+	ARG_VERB_TESTS: {Type: arg.BOOL},
+	ARG_NO_COLOR:   {Type: arg.BOOL},
+	ARG_HELP:       {Type: arg.BOOL, Alias: "u:usage"},
+	ARG_VER:        {Type: arg.BOOL, Alias: "ver"},
+}
+
+var checkPackageImports = []string{
+	"github.com/go-check/check",
+	"gopkg.in/check.v1",
+	"pkg.re/check.v1",
 }
 
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -118,15 +147,21 @@ func process(dir string) {
 		},
 	)
 
-	bImports, tImports, binaries := collectImports(sources, dir)
+	makefile := collectImports(sources, dir)
 
-	bImports = cleanupImports(bImports, dir)
-	tImports = cleanupImports(tImports, dir)
-	binaries = cleanupBinaries(binaries)
+	makefile.Glide = arg.GetB(ARG_GLIDE)
+	makefile.Metalinter = arg.GetB(ARG_METALINTER)
+	makefile.Benchmark = arg.GetB(ARG_BENCHMARK)
+	makefile.VerbTests = arg.GetB(ARG_VERB_TESTS)
 
-	data := generateMakefile(bImports, tImports, binaries)
+	makefile.Cleanup(dir)
 
-	err := ioutil.WriteFile(arg.GetS(ARG_OUTPUT), []byte(data), 0644)
+	exportMakefile(makefile)
+}
+
+// exportMakefile render makefile and write data to file
+func exportMakefile(makefile *Makefile) {
+	err := ioutil.WriteFile(arg.GetS(ARG_OUTPUT), makefile.Render(), 0644)
 
 	if err != nil {
 		printError(err.Error())
@@ -136,92 +171,9 @@ func process(dir string) {
 	fmtc.Printf("{g}Makefile successfully created as {g*}%s{!}\n", arg.GetS(ARG_OUTPUT))
 }
 
-// generateMakefile generate makefile data
-func generateMakefile(bImports, tImports, binaries []string) string {
-	var data string
-
-	data += SEPARATOR + "\n\n"
-	data += ".PHONY = " + generatePhonyPart(bImports, tImports, binaries) + "\n\n"
-	data += SEPARATOR + "\n\n"
-
-	// build section
-	if len(binaries) != 0 {
-		data += "all: " + strings.Join(binaries, " ") + "\n\n"
-
-		for _, bin := range binaries {
-			data += bin + ":\n"
-			data += "\tgo build " + bin + ".go\n\n"
-		}
-	}
-
-	// deps section
-	if len(bImports) != 0 {
-		data += "deps:\n"
-
-		for _, pkg := range bImports {
-			data += "\tgo get -v " + pkg + "\n"
-		}
-
-		data += "\n"
-	}
-
-	// deps-test and test sections
-	if len(tImports) != 0 {
-		data += "deps-test:\n"
-
-		for _, pkg := range tImports {
-			data += "\tgo get -v " + pkg + "\n"
-		}
-
-		data += "\n"
-		data += "test:\n"
-		data += "\tgo test -covermode=count .\n"
-		data += "\n"
-	}
-
-	// fmt section
-	data += "fmt:\n"
-	data += "\tfind . -name \"*.go\" -exec gofmt -s -w {} \\;\n"
-	data += "\n"
-
-	// clean section
-	if len(binaries) != 0 {
-		data += "clean:\n"
-
-		for _, bin := range binaries {
-			data += "\trm -f " + bin + "\n"
-		}
-
-		data += "\n"
-	}
-
-	data += SEPARATOR + "\n\n"
-
-	return data
-}
-
-// generatePhonyPart return phony part for Makefile
-func generatePhonyPart(bImports, tImports, binaries []string) string {
-	phony := []string{"fmt"}
-
-	if len(binaries) != 0 {
-		phony = append(phony, "all", "clean")
-	}
-
-	if len(bImports) != 0 {
-		phony = append(phony, "deps")
-	}
-
-	if len(tImports) != 0 {
-		phony = append(phony, "deps-test", "test")
-	}
-
-	return strings.Join(phony, " ")
-}
-
 // collectImports collect import from source files and return imports for
 // base sources, test sources and slice with binaries
-func collectImports(sources []string, dir string) ([]string, []string, []string) {
+func collectImports(sources []string, dir string) *Makefile {
 	var (
 		bImports map[string]bool
 		tImports map[string]bool
@@ -261,7 +213,12 @@ func collectImports(sources []string, dir string) ([]string, []string, []string)
 		}
 	}
 
-	return importMapToSlice(bImports), importMapToSlice(tImports), binaries
+	return &Makefile{
+		BaseImports: importMapToSlice(bImports),
+		TestImports: importMapToSlice(tImports),
+		Binaries:    binaries,
+		HasTests:    hasTests(sources),
+	}
 }
 
 // splitSources split sources to two slices - with base sources and test sources
@@ -412,6 +369,17 @@ func importMapToSlice(imports map[string]bool) []string {
 	return result
 }
 
+// containsPackage return true if imports contains given packages
+func containsPackage(imports []string, pkgs []string) bool {
+	for _, pkg := range pkgs {
+		if sliceutil.Contains(imports, pkg) {
+			return true
+		}
+	}
+
+	return false
+}
+
 // printError prints error message to console
 func printError(f string, a ...interface{}) {
 	fmtc.Printf("{r}"+f+"{!}\n", a...)
@@ -424,10 +392,222 @@ func printWarn(f string, a ...interface{}) {
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
+// Cleanup clean imports and binaries
+func (m *Makefile) Cleanup(dir string) {
+	m.BaseImports = cleanupImports(m.BaseImports, dir)
+	m.TestImports = cleanupImports(m.TestImports, dir)
+
+	m.Binaries = cleanupBinaries(m.Binaries)
+
+	sort.Strings(m.Binaries)
+}
+
+// Render return makefile data
+func (m *Makefile) Render() []byte {
+	var result string
+
+	result += SEPARATOR + "\n\n"
+	result += m.getPhony() + "\n"
+	result += SEPARATOR + "\n\n"
+
+	result += m.getBinTarget()
+	result += m.getDepsTarget()
+	result += m.getTestTarget()
+	result += m.getGlideTarget()
+	result += m.getFmtTarget()
+	result += m.getMetalinterTarget()
+	result += m.getCleanTarget()
+
+	result += SEPARATOR + "\n\n"
+
+	return []byte(result)
+}
+
+// getPhony return PHONY part of makefile
+func (m *Makefile) getPhony() string {
+	phony := []string{"fmt"}
+
+	if len(m.Binaries) != 0 {
+		phony = append(phony, "all", "clean")
+	}
+
+	if len(m.BaseImports) != 0 {
+		phony = append(phony, "deps")
+	}
+
+	if len(m.TestImports) != 0 {
+		phony = append(phony, "deps-test", "test")
+	}
+
+	if m.Glide {
+		phony = append(phony, "glide-up", "glide-install")
+	}
+
+	if m.Benchmark {
+		phony = append(phony, "benchmark")
+	}
+
+	return ".PHONY = " + strings.Join(phony, " ") + "\n"
+}
+
+// getMetalinterTarget generate target for "all" command and all sub targets
+// for each binary
+func (m *Makefile) getBinTarget() string {
+	if len(m.Binaries) == 0 {
+		return ""
+	}
+
+	var result string
+
+	result += "all: " + strings.Join(m.Binaries, " ") + "\n\n"
+
+	for _, bin := range m.Binaries {
+		result += bin + ":\n"
+		result += "\tgo build " + bin + ".go\n\n"
+	}
+
+	return result
+}
+
+// getMetalinterTarget generate target for "deps" command
+func (m *Makefile) getDepsTarget() string {
+	if len(m.BaseImports) == 0 {
+		return ""
+	}
+
+	var result string
+
+	result += "deps:\n"
+
+	for _, pkg := range m.BaseImports {
+		result += "\tgo get -v " + pkg + "\n"
+	}
+
+	result += "\n"
+
+	return result
+}
+
+// getMetalinterTarget generate target for "test", "deps-test"
+// and "benchmark" commands
+func (m *Makefile) getTestTarget() string {
+	if !m.HasTests {
+		return ""
+	}
+
+	var result string
+
+	if len(m.TestImports) != 0 {
+		result += "deps-test:\n"
+
+		for _, pkg := range m.TestImports {
+			result += "\tgo get -v " + pkg + "\n"
+		}
+
+		result += "\n"
+	}
+
+	result += "test:\n"
+
+	if m.VerbTests {
+		result += "\tgo test -v -covermode=count .\n"
+	} else {
+		result += "\tgo test -covermode=count .\n"
+	}
+
+	result += "\n"
+
+	if m.Benchmark {
+		result += "benchmark:\n"
+
+		if containsPackage(m.TestImports, checkPackageImports) {
+			result += "\tgo test -check.v -check.b -check.bmem\n"
+		} else {
+			result += "\tgo test -bench=.\n"
+		}
+
+		result += "\n"
+	}
+
+	return result
+}
+
+// getMetalinterTarget generate target for "fmt" command
+func (m *Makefile) getFmtTarget() string {
+	var result string
+
+	result += "fmt:\n"
+	result += "\tfind . -name \"*.go\" -exec gofmt -s -w {} \\;\n"
+	result += "\n"
+
+	return result
+}
+
+// getMetalinterTarget generate target for "clean" command
+func (m *Makefile) getCleanTarget() string {
+	if len(m.Binaries) == 0 {
+		return ""
+	}
+
+	var result string
+
+	result += "clean:\n"
+
+	for _, bin := range m.Binaries {
+		result += "\trm -f " + bin + "\n"
+	}
+
+	result += "\n"
+
+	return result
+}
+
+// getMetalinterTarget generate target for "glide-up" and
+// "glide-install" commands
+func (m *Makefile) getGlideTarget() string {
+	if !m.Glide {
+		return ""
+	}
+
+	var result string
+
+	result += "glide-up:\n"
+	result += "\tglide up\n"
+	result += "\n"
+
+	result += "glide-install:\n"
+	result += "\tglide install\n"
+	result += "\n"
+
+	return result
+}
+
+// getMetalinterTarget generate target for "metalineter" command
+func (m *Makefile) getMetalinterTarget() string {
+	if !m.Metalinter {
+		return ""
+	}
+
+	var result string
+
+	result += "metalinter:\n"
+	result += "\ttest -s $GOPATH/bin/metalinter || { go get -u github.com/alecthomas/gometalinter; $GOPATH/bin/metalinter --install }\n"
+	result += "\t$GOPATH/bin/gometalinter --deadline 30s\n"
+	result += "\n"
+
+	return result
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
 //
 func showUsage() {
 	info := usage.NewInfo("", "dir")
 
+	info.AddOption(ARG_GLIDE, "Add target to fetching dependecies with glide")
+	info.AddOption(ARG_METALINTER, "Add target with metalinter check")
+	info.AddOption(ARG_BENCHMARK, "Add target to run benchmarks")
+	info.AddOption(ARG_VERB_TESTS, "Enable verbose output for tests")
 	info.AddOption(ARG_OUTPUT, "Output file {s-}(Makefile by default){!}")
 	info.AddOption(ARG_NO_COLOR, "Disable colors in output")
 	info.AddOption(ARG_HELP, "Show this help message")
